8ccd077cb7b172effe043bb20a303742e596bfb7
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-27 20:16:56 -0400

    Creando algunos metodos de la API prestamo..

---
+++ WebAPI/Controllers/PrestamoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrestamoAPI.BLL;
+using PrestamoAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrestamoController : ControllerBase
+    {
+        // GET: api/Prestamo
+        [HttpGet]
+        public ActionResult<List<Prestamo>> Get()
+        {
+            return PrestamosBLL.GetPrestamos();
+        }
+
+        // GET: api/Prestamo/1
+        [HttpGet("{id}")]
+        public ActionResult<Prestamo> Get(int id)
+        {
+            return PrestamosBLL.Buscar(id);
+        }
+    }
+}
